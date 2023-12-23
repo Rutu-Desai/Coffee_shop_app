3c5bcb7cf711b93487e8a2564d1d2bd6f4be8105
Rutu Desai <rutu.cs21@bmsce.ac.in>
2023-12-24 00:29:44 +0530

    home screen

---
+++ src/components/ImageBackgroundInfo.tsx
@@ -0,0 +1,81 @@
+import { ImageBackground, ImageProps, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react';
+import GradientBGIcon from './GradientBGIcon';
+import { COLORS, FONTSIZE, SPACING } from '../theme/theme';
+
+interface ImageBackgroundInfoProps {
+    EnableBackHandler: boolean;
+    imagelink_portrait: ImageProps;
+    type: string;
+    id: string;
+    favorite: boolean;
+    name: string;
+    special_ingredient: string;
+    ingredients: string;
+    average_rating: number;
+    ratings_count: string;
+    roasted: string;
+    BackHandler?: any;
+    ToggleFavorite: any;
+}
+
+const ImageBackgroundInfo: React.FC<ImageBackgroundInfoProps> = ({
+    EnableBackHandler,
+    imagelink_portrait,
+    type,
+    id,
+    favorite,
+    name,
+    special_ingredient,
+    ingredients,
+    average_rating,
+    ratings_count,
+    roasted,
+    BackHandler,
+    ToggleFavorite,
+}) => {
+  return (
+    <View>
+      <ImageBackground source={imagelink_portrait} style={styles.ItemBackgroundImage} >
+        {EnableBackHandler? 
+            <View style={styles.ImageHeaderBarContainerWithBack}>
+                <TouchableOpacity onPress={BackHandler}>
+                    <GradientBGIcon name={'left'} color={COLORS.primaryLightGreyHex} size={FONTSIZE.size_16} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <GradientBGIcon name={'like'} color={favorite? COLORS.primaryRedHex : COLORS.primaryLightGreyHex} size={FONTSIZE.size_16} />
+                </TouchableOpacity>
+            </View>
+            : 
+            <View style={styles.ImageHeaderBarContainerWithoutBack}>
+            <TouchableOpacity>
+                <GradientBGIcon name={'like'} color={favorite? COLORS.primaryRedHex : COLORS.primaryLightGreyHex} size={FONTSIZE.size_16} />
+            </TouchableOpacity>
+            </View>
+        }
+      </ImageBackground>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    ItemBackgroundImage: {
+        width: '100%',
+        aspectRatio: 20/25,
+        justifyContent: 'space-between',
+    },
+    ImageHeaderBarContainerWithBack: {
+        padding: SPACING.space_30,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    ImageHeaderBarContainerWithoutBack: {
+        padding: SPACING.space_30,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+})
+
+export default ImageBackgroundInfo
